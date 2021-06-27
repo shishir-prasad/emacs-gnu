d32ceaf37ce2de9c9ff6eae13b8eb390d48d5a70
Shishir-iBrew <shishir@ibrew-hub.com>
2021-06-27 20:30:26 +0530

    Personal gnu emacs configuration

---
+++ init.el
@@ -0,0 +1,13 @@
+(setq inhibit-startup-message t)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package) 
+(org-babel-load-file (concat user-emacs-directory "config.org"))
